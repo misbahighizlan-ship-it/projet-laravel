795dfa0c212566583cdf7ddc64124ab9165be146
misbahighizlan-ship-it <misbahighizlan@gmail.com>
2026-07-30 21:51:12 +0100

    migration update

--- database/migrations/2026_07_30_204357_create_products_table.php
+++ database/migrations/2026_07_30_204357_create_products_table.php
@@ -10,12 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->text('description');
+        $table->decimal('price', 10, 2);
+        $table->integer('quantity');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
